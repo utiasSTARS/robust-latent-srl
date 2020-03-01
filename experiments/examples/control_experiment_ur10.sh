a5cdefec39706591d4435f2e6316e49348b0842c
Oliver Limoyo <oliver.limoyo@mail.mcgill.ca>
2020-02-29 21:45:36 -0500

    Clean up arguments for control experiment

--- experiments/examples/control_experiment_ur10.sh
+++ experiments/examples/control_experiment_ur10.sh
@@ -1,10 +1,7 @@
-enable_oods=( 'True' 'False' )
-ood_name="drop_2_clean_2"
-
 python3 ../control_experiment_ur10.py \
     --camera_res=3,480,640 \
-    --dbname="/home/olimoyo/robust-latent-srl/experiments/results/test_tcp.db" \
-    --goal_path="/home/olimoyo/robust-latent-srl/experiments/64x64_white_2_goal.npy" \
+    --dbname="/path/to/outputdb/<storagedbname>.db" \
+    --goal_path="/path/to/goalimage/<filename>.npy" \
     --dt=0.5 \
     --speed_max=0.5 \
     --timeout=30 \
@@ -13,10 +10,10 @@ python3 ../control_experiment_ur10.py \
     --hosts=192.168.42.115 \
     --ports=5000 \
     --num_episodes=9 \
-    --args_output_file="/home/olimoyo/robust-latent-srl/experiments/results/test_tcp_params.pkl" \
+    --args_output_file="/path/to/outputargs/<filename>.pkl" \
     --repeat_actions=3 \
     --seed=0 \
     --render=False \
-    --model_path="/home/olimoyo/robust-latent-srl/srl/saved_models/real-world_reacher/iros/64x64/02-16-20_17:03:55_res64_netgru_constant_a4_z10_trajlen7_wd0_bs64_bnFalse_lr3e-4_R0.03" \
+    --model_path="path/to/load/model" \
     --device=cuda:0 \
     --T=4
